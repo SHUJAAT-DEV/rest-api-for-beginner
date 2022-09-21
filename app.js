b89f9ad8dfc16673651e7ecddab5a195472d6246
shujaat <shujaat.dev@taazaa.com>
2022-09-21 11:45:23 +0500

    added strucuter , jwt token , utiles functions . room,  hotel , user and authentication.

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const app = express();
+const authRoute = require("./routes/auth.js");
+const usersRoute = require("./routes/users.js");
+const hotelsRoute = require("./routes/hotels.js");
+
 dotenv.config();
 
 const connect = async () => {
@@ -19,6 +24,13 @@ mongoose.connection.on("disconnected", () => {
 });
 
 //middlewares
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
 
 app.listen(3001, () => {
   connect();
